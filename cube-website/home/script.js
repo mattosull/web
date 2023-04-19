b512dcb1f1bd2374f24ddc9bf93c1b2895a90698
Matthew O'Sullivan <122844754+mattosull@users.noreply.github.com>
2023-04-19 17:50:41 +1000

    Removed Cube-Website

--- cube-website/home/script.js
+++
@@ -1,7 +0,0 @@
-function sidepanel() {
-    document.getElementById("sidepanel").classList.add("animate");
-  }
-  
-  function close_sidepanel() {
-    document.getElementById("sidepanel").classList.remove("animate");
-  }
